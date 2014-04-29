a402e284fcd7b77a718f93fe914d6696686930dd
xcomSteveJohnson <stevej@x.com>
2014-04-29 08:49:25 +0000

    Remove old guides dir

--- guides/m2devgde/V1.0.0.0/Data/Language.js
+++
@@ -1,1 +0,0 @@
-define({'default':{'OpenBookIconAlternateText':'Open Book','ClosedBookIconAlternateText':'Closed Book','TopicIconAlternateText':'Topic Icon','MarkAsNewIconAlternateText':'New Topic'}});
