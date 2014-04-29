6a56d080b0c562987dc3f23606d94721e0f82082
Steve Johnson <stevjohnson@ebay.com>
2014-04-29 11:56:39 -0500

    Alternate Magento 2 documentation format

---
+++ guides/m2devgde/Test/Data/Toc.js
@@ -0,0 +1,1 @@
+define({numchunks:1,prefix:'Toc_Chunk',chunkstart:['/Content/integration/integration-activate.htm'],tree:{n:[{i:0,c:0,n:[{i:1,c:0},{i:2,c:0}]},{i:3,c:0,n:[{i:4,c:0},{i:5,c:0},{i:6,c:0},{i:7,c:0},{i:8,c:0}]},{i:9,c:0,n:[{i:10,c:0},{i:11,c:0},{i:12,c:0},{i:13,c:0,n:[{i:14,c:0},{i:15,c:0}]},{i:16,c:0,n:[{i:17,c:0}]}]}]}});
