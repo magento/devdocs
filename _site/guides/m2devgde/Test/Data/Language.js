69e4f376b12a446aa8272f096daba1a7c9645d9f
xcomSteveJohnson <stevej@x.com>
2014-04-29 13:15:28 +0000

    Update fonts

---
+++ _site/guides/m2devgde/Test/Data/Language.js
@@ -0,0 +1,1 @@
+define({'default':{'OpenBookIconAlternateText':'Open Book','ClosedBookIconAlternateText':'Closed Book','TopicIconAlternateText':'Topic Icon','MarkAsNewIconAlternateText':'New Topic'}});
