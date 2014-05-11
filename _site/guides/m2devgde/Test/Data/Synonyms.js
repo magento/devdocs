b35e3fef29bf35f19fae0133c9b1ecc83c62e78d
xcomSteveJohnson <stevej@x.com>
2014-05-11 16:39:50 +0000

    Remove old assets

--- _site/guides/m2devgde/Test/Data/Synonyms.js
+++
@@ -1,7 +0,0 @@
-var xmlSynonymsData = "";
-xmlSynonymsData += '<?xml version=\"1.0\" encoding=\"utf-8\"?>';
-xmlSynonymsData += '<MadCapSynonyms>';
-xmlSynonymsData += '    <Groups />';
-xmlSynonymsData += '    <Directional />';
-xmlSynonymsData += '</MadCapSynonyms>';
-MadCap.Utilities.Xhr._FilePathToXmlStringMap.Add('Synonyms', xmlSynonymsData);
--- _site/guides/m2devgde/Test/Resources/Scripts/require.config.js
+++
@@ -1,3 +0,0 @@
-require.config({
-    urlArgs: 't=635339272764847037'
-});
