742b4d89b30fc741f02db165bced82a61aff0de6
xcomSteveJohnson <stevjohnson@ebay.com>
2014-12-17 16:50:40 -0600

    Delete gen_public.bat

--- gen_public.bat
+++
@@ -1,9 +0,0 @@
-echo on
-
-call C:\jekyll\PortableJekyll\setpath
-
-sleep 2
-
-cd C:\Docs\_github-working\private\Magento\_bash\devdocs_internal
-
-jekyll serve -d C:\Docs\_github-working\public\devdocs --config _config.public.yml
