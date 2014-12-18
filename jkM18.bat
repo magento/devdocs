af4d133f007562e23add4b156b9e15763523fd28
Steve Johnson <stevjohnson@ebay.com>
2014-12-18 09:00:42 -0600

    Delete jkM18.bat

--- jkM18.bat
+++
@@ -1,9 +0,0 @@
-echo on
-
-call C:\jekyll\PortableJekyll\setpath
-
-sleep 2
-
-cd C:\Docs\_github-working\private\Magento\_bash\devdocs_internal
-
-jekyll serve --config _config.internalM18.yml -d C:\Docs\_github-working\private\Magento\_bash\gh_devdocs\devdocs_internal
