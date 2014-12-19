c980b016d72a330b03861da4123a9cb293170933
xcomSteveJohnson <stevjohnson@ebay.com>
2014-12-19 14:25:57 -0600

    Update Release Notes and system requirements

---
+++ jk.bat
@@ -0,0 +1,9 @@
+echo on
+
+call C:\jekyll\PortableJekyll\setpath
+
+sleep 2
+
+cd C:\Docs\_github-working\private\Magento\_bash\devdocs_internal
+
+jekyll serve --baseurl '/'  --watch --config _config.steve.yml
