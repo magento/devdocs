04bd280f8d41dc1b75079ee445e2cd7b6cfa763f
T Dobbs <tdobbs@ebay.com>
2015-09-14 12:47:37 -0500

    Search function updated.

--- common/js/devdocs.js
+++ common/js/devdocs.js
@@ -1,12 +1,50 @@
 function devdocs(){
 
 
+if ($.cookie('searchResultsState') ==1) {
+	//show search results page
+	$('#searchResultsBox').show(300);
+	$('.searchResultsBack').show(1200);
+} else {
+	$.cookie('searchResultsState', '0');
+	$('#searchResultsBox').hide(300);
+	$('.searchResultsBack').hide(1200);
+}
+
+
+$('form').submit(function(event) {
+	console.log("submit called");
+	$('#searchResultsBox').show(300);
+	$('.searchResultsBack').show(1200);
+	$.cookie('searchResultsState', '1');
+	
+});
+
+$('DIV.searchResultsBoxClose').click(function(){
+	$('#searchResultsBox').hide(300);
+	$('.searchResultsBack').hide(1200);
+	$.cookie('searchResultsState','0');
+    console.log($.cookie('searchResultsState'));
+});
+
+//first load the page, searchResultsState = 0 search results hidden.
+// on submit searchResultState = 1, cookie set to 1 search results shown.
+// close search window, searchResultState = 0 search results hidden.
+
+
+
+
+
+
+
 console.log('devdocs is up!');
 
+$(document.body).on('click', '#test' ,function(){
 
-$('FORM#searchbox').keyup(function(){ 
-    console.log('form Search results shown');
+    console.log('These are in fact the droids that you are looking for.');
 });
+
+
 
 
 }   //end devdocs
