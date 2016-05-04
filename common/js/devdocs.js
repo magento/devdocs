7cb679c9342f6517dfe9b9dde7db9845aa136130
James Calcaben <jcalcaben@magento.com>
2016-05-04 12:42:44 -0500

    Define accordion behavior via js options

--- common/js/devdocs.js
+++ common/js/devdocs.js
@@ -41,7 +41,12 @@ $(document).ready(function(){
 	});
 
 	//All divs with classname "collapsible" will have jquery-ui accordion functionality
-	$(".collapsible").accordion();
+	$(".collapsible").accordion({
+		collapsible: true,
+		active: false,
+		icons: { "header": "collapsible-ready", "activeHeader": "collapsible-active" },
+		header: ".collapsible-title"
+	});
 
 });
 
