f7a58edd9c22309523c713be217dfabe8499f7c6
James Calcaben <jcalcaben@magento.com>
2016-06-22 15:41:45 -0500

    Append the site.baseurl to the page.baseurl variable

--- _plugins/page-baseurl-generator.rb
+++ _plugins/page-baseurl-generator.rb
@@ -2,6 +2,7 @@ module PageBaseUrlGenerator
   class Generator < Jekyll::Generator
     def generate(site)
       pages = site.pages
+      siteBaseUrl = site.baseurl
       version = "2.1"
       for page in pages
         destination = page.path
@@ -11,7 +12,7 @@ module PageBaseUrlGenerator
           version = matcher[1]
         end
 
-        page.data['baseurl'] = "/guides/v#{version}/"
+        page.data['baseurl'] = "#{siteBaseUrl}guides/v#{version}/"
         page.data['version'] = version
       end
     end
