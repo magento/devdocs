4b4f59e2a5422cdfc139821eb588f31129bf3ace
Dima Shevtsov <dshevtsov@magento.com>
2018-06-04 01:46:25 -0500

    Refactored Ruby syntax (Rubocop)

--- _plugins/collapsible-content.rb
+++ _plugins/collapsible-content.rb
@@ -1,12 +1,11 @@
 # Author: jcalcaben@magento.com
 #
-# This custom plugin adds a block tag that wraps the content in a jquery-ui accordion
-# widget.
+# This custom plugin adds a block tag that wraps the content in
+# a jquery-ui accordion widget.
 #
-
 module Jekyll
+  # Collabsibility for regular blocks
   class Collapsible < Liquid::Block
-
     def initialize(tag_name, title, tokens)
       super
       @title = title.to_s
@@ -15,75 +14,79 @@ module Jekyll
     def render(context)
       site = context.registers[:site]
 
-      if defined? site.find_converter_instance
-        converter = site.find_converter_instance(Jekyll::Converters::Markdown)
-      else
-        converter = site.getConverterImpl(::Jekyll::Converters::Markdown)
-      end
+      converter = if defined? site.find_converter_instance
+                    site.find_converter_instance(Jekyll::Converters::Markdown)
+                  else
+                    site.getConverterImpl(::Jekyll::Converters::Markdown)
+                  end
 
       content = super.strip
       content = converter.convert(content)
-      "<div class=\"collapsible\"><b class=\"collapsible-title\">"+@title+"</b><div class=\"collapsible-content\">"+content+"</div></div>"
+      <<-HTML
+  <div class="collapsible">
+    <b class="collapsible-title">#{@title}</b>
+    <div class="collapsible-content">#{content}</div>
+  </div>
+        HTML
+    end
+  end
+
+  ## TODO: Come up with cleaner solution
+  # Collabsibility as headings of level 2
+  class CollapsibleH2 < Liquid::Block
+    def initialize(tag_name, title, tokens)
+      super
+      @title = title.to_s
+    end
+
+    def render(context)
+      site = context.registers[:site]
+
+      converter = if defined? site.find_converter_instance
+                    site.find_converter_instance(Jekyll::Converters::Markdown)
+                  else
+                    site.getConverterImpl(::Jekyll::Converters::Markdown)
+                  end
+
+      content = super.strip
+      content = converter.convert(content)
+      <<-HTML
+  <div class="collapsible collapsible-h2">
+    <h2 class="collapsible-title">#{@title}</h2>
+    <div class="collapsible-content">#{content}</div>
+  </div>
+        HTML
+    end
+  end
+
+  # Collabsibility as headings of level 3
+  class CollapsibleH3 < Liquid::Block
+    def initialize(tag_name, title, tokens)
+      super
+      @title = title.to_s
+    end
+
+    def render(context)
+      site = context.registers[:site]
+
+      converter = if defined? site.find_converter_instance
+                    site.find_converter_instance(Jekyll::Converters::Markdown)
+                  else
+                    site.getConverterImpl(::Jekyll::Converters::Markdown)
+                  end
+
+      content = super.strip
+      content = converter.convert(content)
+      <<-HTML
+  <div class="collapsible collapsible-h3">
+    <h3 class="collapsible-title">#{@title}</h3>
+    <div class="collapsible-content">#{content}</div>
+  </div>
+        HTML
     end
   end
 end
 
 Liquid::Template.register_tag('collapsible', Jekyll::Collapsible)
-
-
-
-## TODO: Come up with cleaner solution
-
-module Jekyll
-  class CollapsibleH2 < Liquid::Block
-
-    def initialize(tag_name, title, tokens)
-      super
-      @title = title.to_s
-    end
-
-    def render(context)
-      site = context.registers[:site]
-
-      if defined? site.find_converter_instance
-        converter = site.find_converter_instance(Jekyll::Converters::Markdown)
-      else
-        converter = site.getConverterImpl(::Jekyll::Converters::Markdown)
-      end
-
-      content = super.strip
-      content = converter.convert(content)
-      "<div class=\"collapsible collapsible-h2\"><h2 class=\"collapsible-title\">"+@title+"</h2><div class=\"collapsible-content\">"+content+"</div></div>"
-    end
-  end
-end
-
 Liquid::Template.register_tag('collapsibleh2', Jekyll::CollapsibleH2)
-
-
-
-module Jekyll
-  class CollapsibleH3 < Liquid::Block
-
-    def initialize(tag_name, title, tokens)
-      super
-      @title = title.to_s
-    end
-
-    def render(context)
-      site = context.registers[:site]
-
-      if defined? site.find_converter_instance
-        converter = site.find_converter_instance(Jekyll::Converters::Markdown)
-      else
-        converter = site.getConverterImpl(::Jekyll::Converters::Markdown)
-      end
-
-      content = super.strip
-      content = converter.convert(content)
-      "<div class=\"collapsible collapsible-h3\"><h3 class=\"collapsible-title\">"+@title+"</h3><div class=\"collapsible-content\">"+content+"</div></div>"
-    end
-  end
-end
-
 Liquid::Template.register_tag('collapsibleh3', Jekyll::CollapsibleH3)
