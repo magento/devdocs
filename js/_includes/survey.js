63644d6fd9e42106332c559c584806089b7362a4
Eugene Bannykh <ybannykh@magento.com>
2018-06-21 16:46:38 -0500

    Add survey banner to the home page and internal pages

---
+++ js/_includes/survey.js
@@ -0,0 +1,36 @@
+// Create a survey banner that dissapears forever once you dismiss it
+
+$(function() {
+
+  // Fist, check if browser supoprts storing the data
+  if (typeof localStorage !== 'undefined') {
+
+    // Only show if user didnt interact with the banner
+    if ( !localStorage.getItem("Survey done") ) {
+
+      // Build banner text
+      var $banner = $('<div class="bs-callout" style="display:none; text-align:left; max-width: 1024px;"><h2>Input needed from first-time Magento developers</h2><p>If you’re just starting, or have been developing in Magento for 6 months or less, we want to talk with you. If you’re interested in sharing your experiences with us, please fill out the short survey, and we’ll schedule a time to chat.</p> <button data-href="https://www.surveymonkey.com/r/new2magento" class="btn btn-primary btn-large">OK, I’ll help</button> <button class="btn btn-large">No, thanks</button> </div>');
+
+      // Assign events
+      $banner.find('a, button').on('click', function () {
+
+        var $this = $(this);
+
+        localStorage.setItem("Survey done" , true);
+        $banner.delay(100).slideUp(400);
+
+        if ($this.data('href')) {
+          window.open($this.data('href'), '_blank');
+        }
+
+      });
+
+      // Insert banner in it's place
+      $banner.insertBefore('.home-features .feature:first');
+      $banner.appendTo('.content-wrap');
+
+      // Show banner
+      $banner.delay(500).slideDown(400);
+    }
+  }
+});
