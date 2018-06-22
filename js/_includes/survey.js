ad5f20468df011941336c99582ea3c476e03cd87
Eugene Bannykh <ybannykh@magento.com>
2018-06-22 17:40:05 -0500

    Rename banner title to “New to Magento?”

--- js/_includes/survey.js
+++ js/_includes/survey.js
@@ -9,7 +9,7 @@ $(function() {
     if ( !localStorage.getItem("Survey done") ) {
 
       // Build banner text
-      var $banner = $('<div class="bs-callout" style="display:none; text-align:left; max-width: 1024px;"><h2>Input needed from first-time Magento developers</h2><p>If you are just starting or have been developing in Magento for 6 months or less, we want to talk to you. If you are interested in sharing your experiences with us, please fill out the short survey and we will schedule a time to chat.</p> <button data-href="https://www.surveymonkey.com/r/new2magento" class="btn btn-primary btn-large">OK, I will help</button> <button class="btn btn-large">No, thanks</button> </div>');
+      var $banner = $('<div class="bs-callout" style="display:none; text-align:left; max-width: 1024px;"><h2>New to Magento?</h2><p>If you are just starting or have been developing in Magento for 6 months or less, we want to talk to you. If you are interested in sharing your experiences with us, please fill out the short survey and we will schedule a time to chat.</p> <button data-href="https://www.surveymonkey.com/r/new2magento" class="btn btn-primary btn-large">OK, I will help</button> <button class="btn btn-large">No, thanks</button> </div>');
 
       // Assign events
       $banner.find('a, button').on('click', function () {
