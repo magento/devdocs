6eec70b0f5c363b7c04486ad11ccebc4bfccaa21
Eugene Bannykh <ybannykh@magento.com>
2018-06-22 10:28:21 -0500

    Correct spelling for banner text

--- js/_includes/survey.js
+++ js/_includes/survey.js
@@ -1,15 +1,15 @@
-// Create a survey banner that dissapears forever once you dismiss it
+// Create a survey banner that disappears forever once you dismiss it
 
 $(function() {
 
-  // Fist, check if browser supoprts storing the data
+  // Fist, check if browser supports storing the data
   if (typeof localStorage !== 'undefined') {
 
-    // Only show if user didnt interact with the banner
+    // Only show if user didn't interact with the banner
     if ( !localStorage.getItem("Survey done") ) {
 
       // Build banner text
-      var $banner = $('<div class="bs-callout" style="display:none; text-align:left; max-width: 1024px;"><h2>Input needed from first-time Magento developers</h2><p>If you’re just starting, or have been developing in Magento for 6 months or less, we want to talk with you. If you’re interested in sharing your experiences with us, please fill out the short survey, and we’ll schedule a time to chat.</p> <button data-href="https://www.surveymonkey.com/r/new2magento" class="btn btn-primary btn-large">OK, I’ll help</button> <button class="btn btn-large">No, thanks</button> </div>');
+      var $banner = $('<div class="bs-callout" style="display:none; text-align:left; max-width: 1024px;"><h2>Input needed from first-time Magento developers</h2><p>If you are just starting or have been developing in Magento for 6 months or less, we want to talk to you. If you are interested in sharing your experiences with us, please fill out the short survey and we will schedule a time to chat.</p> <button data-href="https://www.surveymonkey.com/r/new2magento" class="btn btn-primary btn-large">OK, I will help</button> <button class="btn btn-large">No, thanks</button> </div>');
 
       // Assign events
       $banner.find('a, button').on('click', function () {
@@ -19,13 +19,13 @@ $(function() {
         localStorage.setItem("Survey done" , true);
         $banner.delay(100).slideUp(400);
 
-        if ($this.data('href')) {
-          window.open($this.data('href'), '_blank');
+        if ( $this.data('href') ) {
+          window.open( $this.data('href'), '_blank' );
         }
 
       });
 
-      // Insert banner in it's place
+      // Insert banner in place
       $banner.insertBefore('.home-features .feature:first');
       $banner.appendTo('.content-wrap');
 
