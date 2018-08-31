163334cbb658317938fc167ada44d8bf5a90d05d
Dima Shevtsov <shevtsov@adobe.com>
2018-08-31 16:45:12 -0500

    Extended the page-baseurl-generator Now it reads guide_version from front matter first

--- _plugins/page-baseurl-generator.rb
+++ _plugins/page-baseurl-generator.rb
@@ -5,7 +5,10 @@
 #
 #
 module Jekyll
-  # Extending the Generator class of the Jekyll module
+  # The class generates page.baseurl as "{site.baseurl}/guides/v#{version}".
+  # The {version} is taken from the 'guide_version' front matter parameter on the page;
+  # if it is not set, then the generator takes it from the path of the page (for example, "2.2" in the "guides/v2.2");
+  # if the path doesn't contain "guides/vx.x", then the version is unset (returns nil object, same as null)
   class PageBaseUrlGenerator < Generator
     def generate(site)
       pattern = %r{guides\/v(\d\.\d)}
@@ -13,7 +16,12 @@ module Jekyll
       baseurl = site.baseurl
       pages.each do |page|
         matcher = pattern.match(page.path)
-        version = matcher[1] if matcher
+        version =
+          if page.data['guide_version']
+            page.data['guide_version']
+          elsif matcher
+            matcher[1]
+          end
         page.data['baseurl'] = if version
                                  "#{baseurl}/guides/v#{version}"
                                else
