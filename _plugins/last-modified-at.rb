1dbd7bbc8300fbcf514f991f4a2f433ea926a3e2
Dima Shevtsov <shevtsov@adobe.com>
2019-01-08 22:41:05 -0600

    Resolve symlinks for last-modified-at

--- _plugins/last-modified-at.rb
+++ _plugins/last-modified-at.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 #
 # This custom plugin dynamically sets the 'last_modified_at' parameter
 # for each page except 'redirect.html' pages.
-# The parameter contains date and time of the last commit that changed the corresponding file.
+# A value of the pararmeter is available as {{ page.last_modified_at }}.
+# The parameter contains date and time of the last commit that changed
+# the original file.
 # For available date formats, refer to https://git-scm.com/docs/git-log#git-log---dateltformatgt
 #
 Jekyll::Hooks.register :pages, :post_init do |page|
@@ -11,7 +15,13 @@ Jekyll::Hooks.register :pages, :post_init do |page|
   # Do nothing for redirects
   next if page.name == 'redirect.html'
 
-  # Create a page parameter 'last_modified_at' and assign a value to it
+  page_path = page.path
+
+  # If the page is a symlink, use path of the original file
+  page_path = File.realpath page_path if File.symlink? page_path
+
+  # Read date of the last committ and assign it to last_modified_at parameter
+  # of the page.
   page.data['last_modified_at'] =
-    `git log -1 --format=%cd --date=iso -- #{page.path}`.strip
+    `git log -1 --format=%cd --date=iso -- #{page_path}`.strip
 end
