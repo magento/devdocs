7b6cd43f06c63625aa323a115636afce7e38ea6f
Jeff Matthews <matthews.jeffery@gmail.com>
2019-02-01 15:06:04 -0600

    MAGEDOC-3450: Simplify script

--- scripts/page-builer.sh
+++ scripts/page-builer.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 set -x
-ls >> _config.page-builder.yml
-sed -i '' -ne '/^[^_]/p' _config.page-builder.yml
-sed -i '' -ne '/^page-builder/!p' _config.page-builder.yml
-sed -i '' -e 's/^/- /' _config.page-builder.yml
-sed -i '' -e '1s;^;exclude:\
-;' _config.page-builder.yml
+rm 404.md
+rm availability.md
+rm -rf community
+rm -rf guides
+rm index.html
+rm magento-techbull.md
+rm magento-third-party.md
+rm -rf mftf
+rm -rf redoc
+rm -rf schemas
+rm -rf swagger
+rm search.md
+
