68d8b475daa0acd65899ae665c16a0575d50bc5c
Jeff Matthews <matthews.jeffery@gmail.com>
2019-01-31 18:17:28 -0600

    MAGEDOC-3450: Move partial build commands to bash script

---
+++ scripts/page-builer.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+ls >> _config.page-builder.yml
+sed -i '' -ne '/^[^_]/p' _config.page-builder.yml
+sed -i '' -ne '/^page-builder/!p' _config.page-builder.yml
+sed -i '' -e 's/^/- /' _config.page-builder.yml
+sed -i '' -e '1s;^;exclude:\
+;' _config.page-builder.yml
