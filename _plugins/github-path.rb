0b0d77c6f4b9e4578bd6a365ba98166331b2c261
Dima Shevtsov <shevtsov@adobe.com>
2019-03-01 18:15:12 -0600

    Update docs

--- _plugins/github-path.rb
+++ _plugins/github-path.rb
@@ -1,15 +1,12 @@
 # frozen_string_literal: true
 
 #
-# This custom plugin dynamically sets the 'last_modified_at' parameter
+# This custom plugin dynamically sets the 'github_path' parameter
 # for each page except 'redirect.html' pages.
 # A value of the pararmeter is available as {{ page.last_modified_at }}.
-# The parameter contains date and time of the last commit that changed
-# the original file.
-# For available date formats, refer to https://git-scm.com/docs/git-log#git-log---dateltformatgt
+# The parameter contains a file path relative to its repository.
 #
 Jekyll::Hooks.register :pages, :post_init do |page|
-  # Do nothing in serving mode
   # Process only files with 'md' and 'html' extensions
   next unless File.extname(page.path).match?(/md|html/)
   # Do nothing for redirects
@@ -18,8 +15,8 @@ Jekyll::Hooks.register :pages, :post_init do |page|
   dir = File.dirname page.path
   filename = File.basename page.path
 
-  # Read date of the last committ and assign it to last_modified_at parameter
-  # of the page.
+  # Change to the parent directory of the page and read full file path
+  # from git index.
   page.data['github_path'] =
     `cd #{dir} && git ls-files --full-name #{filename}`.strip
 end
