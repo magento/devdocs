cd0d02fba8586f63a1cb7bc0395c17307e8a9c9e
Dima Shevtsov <shevtsov@adobe.com>
2019-03-15 21:36:01 -0500

    Changed each item to hash with name and url
    To be compatible with the existing implementation of version switcher

--- _plugins/page-versions.rb
+++ _plugins/page-versions.rb
@@ -3,11 +3,11 @@
 #
 # This custom plugin dynamically sets the 'versions' parameter
 # for page at 'guides/v2.x' directories.
-# A value of the pararmeter is an array and is available as {{ page.versions }}.
-# The parameter contains relative URLs of the topic in all versions where it exists.
+# The pararmeter is available as a liquid expression {{ page.versions }}.
+# It stores an array of hashes like [ { 'name' => '2.1', 'url' = guides/v2.1/index.html }, { 'name' => '2.2', 'url' = guides/v2.2/index.html }, etc ].
+# The parameter contains only exisiting pages.
 #
 Jekyll::Hooks.register :pages, :pre_render do |page, config|
-
   # Process only files with 'md' and 'html' extensions
   next unless File.extname(page.path).match?(/md|html/)
 
@@ -30,9 +30,22 @@ Jekyll::Hooks.register :pages, :pre_render do |page, config|
 
   non_version_path = url.sub(%r{#{filtering_pattern}[^/]+}, '')
 
-  versions = []
+  version_urls = []
 
-  versions += urls_filtered_by_pattern.select { |path| path.end_with? non_version_path }
+  version_urls += urls_filtered_by_pattern.select { |path| path.end_with? non_version_path }
 
-  config['page']['versions'] = versions.sort
+  version_pattern = %r{\A#{filtering_pattern}(?<version_from_path>[^/]+)}
+
+  versions =
+    version_urls.map do |v_url|
+      version_pattern.match(v_url)
+      version = Regexp.last_match(:version_from_path)
+
+      {
+        'name' => version,
+        'url' => v_url
+      }
+    end
+
+  config['page']['versions'] = versions.sort_by { |version| version['name'] }
 end
