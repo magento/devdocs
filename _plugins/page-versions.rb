4f43a12987186ca18ced76af926893b339c5e1e0
Dima Shevtsov <shevtsov@adobe.com>
2019-03-19 16:23:34 -0500

    Update page-versions plugin
    Fix a bug for cases when nonversioned path is empty like '/guides/v2.2/'

--- _plugins/page-versions.rb
+++ _plugins/page-versions.rb
@@ -2,10 +2,10 @@
 
 #
 # This custom plugin dynamically sets the 'versions' parameter
-# for page at 'guides/v2.x' directories.
+# for a page at any '/guides/v<...>/' directory.
 # The pararmeter is available as a liquid expression {{ page.versions }}.
 # It stores an array of hashes like [ { 'name' => '2.1', 'url' = guides/v2.1/index.html }, { 'name' => '2.2', 'url' = guides/v2.2/index.html }, etc ].
-# The parameter contains only exisiting pages.
+# The parameter contains all available versions of the topic.
 #
 Jekyll::Hooks.register :pages, :pre_render do |page, config|
   # Process only files with 'md' and 'html' extensions
@@ -18,34 +18,47 @@ Jekyll::Hooks.register :pages, :pre_render do |page, config|
   filtering_pattern = '/guides/v'
   next unless page.url.start_with? filtering_pattern
 
+  # Get all page objects at the site
   pages = page.site.pages
 
+  # Select pages that do not have name 'redirect.html' and their URL
+  # starts with '/guides/v'. Get 'url' of each page and store them as an array.
   urls_filtered_by_pattern =
-    pages.select do |site_page|
+    pages.filter do |site_page|
       next if site_page.name == 'redirect.html'
       site_page.url.start_with? filtering_pattern
     end.map(&:url)
 
   url = page.url
 
-  non_version_path = url.sub(%r{#{filtering_pattern}[^/]+}, '')
+  # Get the nonversion path from URL removing prefix 'guides/v<...>/'.
+  versioned_prefix_pattern = %r{\A#{filtering_pattern}[^/]+}
+  non_version_path = url.sub(versioned_prefix_pattern, '')
 
-  version_urls = []
+  # Define a regular expression to match all versions of a topic
+  full_path_pattern = /#{versioned_prefix_pattern}#{non_version_path}\Z/
 
-  version_urls += urls_filtered_by_pattern.select { |path| path.end_with? non_version_path }
+  # Get URLs for all versions of the topic
+  versioned_urls =
+    urls_filtered_by_pattern.filter { |path| path.match full_path_pattern }
 
+  # Define a regular expression to get a version number from URL
+  # to the 'version_from_path' variable
   version_pattern = %r{\A#{filtering_pattern}(?<version_from_path>[^/]+)}
 
+  # Get all versioned URLs of the topic into array of key-value pairs
+  # like { 'name' => '2.0', 'url' => '/guides/v2.3/index.html' }
   versions =
-    version_urls.map do |v_url|
+    versioned_urls.map do |v_url|
       version_pattern.match(v_url)
       version = Regexp.last_match(:version_from_path)
-
       {
         'name' => version,
         'url' => v_url
       }
     end
 
+  # Set the page.versions parameter to sorted array of key-value pairs
+  # from 'versions'
   config['page']['versions'] = versions.sort_by { |version| version['name'] }
 end
