d5ab5e905cf2532b9b8f4b6681ed694ed0675050
Dima Shevtsov <shevtsov@adobe.com>
2019-10-02 12:02:29 -0500

    Add github_path to the mrg pages

--- _plugins/generators/mrg_pages.rb
+++ _plugins/generators/mrg_pages.rb
@@ -50,6 +50,7 @@ module Jekyll
           )
           mrg_topic.content = metadata['content']
           mrg_topic.data['title'] = metadata['title']
+          mrg_topic.data['github_path'] = metadata['github_path']
           mrg_topic.data['last_modified_at'] = metadata['last_modified_at']
           mrg_topic.process("#{mod}.md")
 
