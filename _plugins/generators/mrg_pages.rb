fa311bec27eb8d1b3edf64adfd4bf205553b2ae0
Dima Shevtsov <shevtsov@adobe.com>
2020-02-28 16:16:08 -0600

    Move the mrg reference data to a versioned dir

--- _plugins/generators/mrg_pages.rb
+++ _plugins/generators/mrg_pages.rb
@@ -21,11 +21,11 @@ module Jekyll
 
       # Data from '_data/codebase/mrg' as a Hash where
       # the filename is a key and its content is a value.
-      mrg_data = @site.data['codebase']['mrg']
+      mrg_data = @site.data['codebase']['v2_3']['mrg']
       # Loop through the hash where a key is assigned to a 'mod' (module is a
       # special token in Ruby and should not be used) and value is assigned to
       # 'metadata'.
-      # For example, for '_data/codebase/mrg/NewModule.yml' that contains
+      # For example, for '_data/codebase/v2_3/mrg/NewModule.yml' that contains
       #
       #         title: Magento_NewModule
       #         edition: ce
