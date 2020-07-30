d5bbbd8d2a2ebe3f367b626a2a34c6ced5f94257
Dima Shevtsov <shevtsov@adobe.com>
2020-07-29 19:08:53 -0500

    Assign page.guide_version in config scopes

--- _plugins/page-params/page-baseurl-generator.rb
+++ _plugins/page-params/page-baseurl-generator.rb
@@ -15,20 +15,11 @@
 # if the path doesn't contain "guides/v2.x", then the version is unset and returns the nil object (same as null)
 Jekyll::Hooks.register :pages, :post_init do |page|
   # Glossary. Create and assign variables to be used in the script.
-  pattern = %r{\Aguides/v(?<version_from_path>2\.\d)}
   baseurl = page.site.baseurl
-  path = page.path
   data = page.data
   version = data['guide_version']
 
-  # Process 'page.guide_version' pararmeter
-  if version.nil? && path.start_with?('guides')
-    pattern.match(path)
-    version = Regexp.last_match(:version_from_path)
-    data['guide_version'] = version
-  end
-
-  # Process 'page.baseurl' parameter
+  # Create 'page.baseurl' parameter
   data['baseurl'] =
     if version
       "#{baseurl}/guides/v#{version}"
