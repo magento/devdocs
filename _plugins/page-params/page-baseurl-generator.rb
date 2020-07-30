aaf0efaae010ffefbd5614e391c29adb6d7f5d5f
Dima Shevtsov <shevtsov@adobe.com>
2020-07-29 19:22:38 -0500

    Update documentation

--- _plugins/page-params/page-baseurl-generator.rb
+++ _plugins/page-params/page-baseurl-generator.rb
@@ -6,13 +6,12 @@
 # This custom plugin dynamically sets and injects the page.baseurl variable
 # based on the page's destination.
 #
-# The hook introduces the page.baseurl and
-# page.guide_version data parameters for each page.
+# The hook introduces the page.baseurl parameter for each page.
 # For pages at guides/v2.x the page.baseurl parameter is set
 # as "{site.baseurl}/guides/v#{version}".
 # The {version} is taken from the 'guide_version' front matter parameter on the page;
-# if it is not set, then the 'guide_version' is set to version from the page path (for example, "2.2" in the "guides/v2.2/**/*.md");
-# if the path doesn't contain "guides/v2.x", then the version is unset and returns the nil object (same as null)
+# if it is not set, then the 'guide_version' is set to version from the _config.yml depending on a scope by path (for example, "2.4" at the "guides/v2.4/");
+# otherwise, the version is unset and returns the nil object (same as null).
 Jekyll::Hooks.register :pages, :post_init do |page|
   # Glossary. Create and assign variables to be used in the script.
   baseurl = page.site.baseurl
