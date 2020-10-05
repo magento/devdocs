e9dcc705eae42a696a4ccde61562dba1943d01be
Dima Shevtsov <shevtsov@adobe.com>
2020-10-05 13:04:03 -0500

    Add plugins for debugging

---
+++ _plugins/debug/site_post_render.rb
@@ -0,0 +1,38 @@
+# Copyright © Magento, Inc. All rights reserved.
+# See COPYING.txt for license details.
+
+# To enable this plugin, add to your 'config.local.yml' the following:
+# 
+# debug: site_post_render
+# 
+# This plugin runs an IRB session (https://github.com/ruby/irb) of a Jekyll application in a serving mode when it's at a state after rendering the whole site, but before writing any files.
+# See the ":site, :post_render" hook: https://jekyllrb.com/docs/plugins/hooks/
+# Available objects to explore are 'site' and 'config'.
+#
+# Several helpful methods (to use a method, chain to an object such as 'site.methods', 'config.keys'):
+# - '.methods.sort'
+# - '.instance_variables.sort'
+# - '.keys.sort'
+#
+# Examples:
+# 
+# To view available configuration data of the site
+# > config.site.keys
+# 
+# To view the number of pages:
+# > config.site.pages.count
+# 
+# To find a page by path and view its data:
+# > page = config.site.pages.select { |page| page.path == 'cloud/env/variables-build.md' }[0] 
+# > page.data
+#
+# To exit from the IRB session:
+# > exit!
+# 
+Jekyll::Hooks.register :site, :post_render do |site, config|
+  next unless page.site.config['serving']
+
+  if site.config['debug'] == 'site_post_render'
+    binding.irb
+  end
+end
