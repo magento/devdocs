53e23770288f7e41177b5e2140204712a6ba4c0c
Dima Shevtsov <shevtsov@adobe.com>
2020-10-05 13:10:20 -0500

    Rename config to payload in plugins for debugging

--- _plugins/debug/site_post_render.rb
+++ _plugins/debug/site_post_render.rb
@@ -1,15 +1,15 @@
 # Copyright © Magento, Inc. All rights reserved.
 # See COPYING.txt for license details.
 
-# To enable this plugin, add to your 'config.local.yml' the following:
+# To enable this plugin, add to your '_config.local.yml' the following:
 # 
 # debug: site_post_render
 # 
 # This plugin runs an IRB session (https://github.com/ruby/irb) of a Jekyll application in a serving mode when it's at a state after rendering the whole site, but before writing any files.
 # See the ":site, :post_render" hook: https://jekyllrb.com/docs/plugins/hooks/
-# Available objects to explore are 'site' and 'config'.
+# Available objects to explore are 'site' and 'payload'.
 #
-# Several helpful methods (to use a method, chain to an object such as 'site.methods', 'config.keys'):
+# Several helpful methods (to use a method, chain to an object such as 'site.methods', 'payload.keys'):
 # - '.methods.sort'
 # - '.instance_variables.sort'
 # - '.keys.sort'
@@ -17,13 +17,13 @@
 # Examples:
 # 
 # To view available configuration data of the site
-# > config.site.keys
+# > payload.site.keys
 # 
 # To view the number of pages:
-# > config.site.pages.count
+# > payload.site.pages.count
 # 
 # To find a page by path and view its data:
-# > page = config.site.pages.select { |page| page.path == 'cloud/env/variables-build.md' }[0] 
+# > page = payload.site.pages.select { |page| page.path == 'cloud/env/variables-build.md' }[0] 
 # > page.data
 #
 # To exit from the IRB session:
--- _plugins/debug/site_pre_render.rb
+++ _plugins/debug/site_pre_render.rb
@@ -1,15 +1,15 @@
 # Copyright © Magento, Inc. All rights reserved.
 # See COPYING.txt for license details.
 
-# To enable this plugin, add to your 'config.local.yml' the following:
+# To enable this plugin, add to your '_config.local.yml' the following:
 # 
 # debug: site_pre_render
 # 
 # This plugin runs an IRB session (https://github.com/ruby/irb) of a Jekyll application in a serving mode when it's at a state just before rendering the whole site.
 # See the ":site, :pre_render" hook: https://jekyllrb.com/docs/plugins/hooks/
-# Available objects to explore are 'site' and 'config'.
+# Available objects to explore are 'site' and 'payload'.
 #
-# Several helpful methods (to use a method, chain to an object such as 'site.methods', 'config.keys'):
+# Several helpful methods (to use a method, chain to an object such as 'site.methods', 'payload.keys'):
 # - '.methods.sort'
 # - '.instance_variables.sort'
 # - '.keys.sort'
@@ -17,19 +17,19 @@
 # Examples:
 # 
 # To view available configuration data of the site
-# > config.site.keys
+# > payload.site.keys
 # 
 # To view the number of pages:
-# > config.site.pages.count
+# > payload.site.pages.count
 # 
 # To find a page by path and view its data:
-# > page = config.site.pages.select { |page| page.path == 'cloud/env/variables-build.md' }[0] 
+# > page = payload.site.pages.select { |page| page.path == 'cloud/env/variables-build.md' }[0] 
 # > page.data
 # 
 # To exit from the IRB session:
 # > exit!
 # 
-Jekyll::Hooks.register :site, :pre_render do |site, config|
+Jekyll::Hooks.register :site, :pre_render do |site, payload|
   next unless page.site.config['serving']
 
   if site.config['debug'] == 'site_pre_render'
