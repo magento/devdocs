4291001edf6a526f8b42b393085f3838316a33ce
Dmytro Shevtsov <shevtsov@adobe.com>
2022-12-06 15:45:39 -0600

    Update migrated_log generator
    Review and fix the migration metadata
    Add the migration metadata to global data

--- _plugins/generators/migrated_log.rb
+++ _plugins/generators/migrated_log.rb
@@ -3,7 +3,7 @@
 # Copyright © Adobe, Inc. All rights reserved.
 # See COPYING.txt for license details.
 
-# This plugin generates the page that contains a list of migrated topics.
+# This plugin generates the page that contains a list of migrated topics: https://devdocs.magento.com/migrated.html
 #
 
 module Jekyll
@@ -12,13 +12,17 @@ module Jekyll
     safe true
 
     def generate(site)
-      # Make the site object available in any scope in this class.
       @site = site
       pages = @site.pages
-      migrated_pages = pages.filter { |page| page.data['layout'] and page.data['layout'].include? 'migrated' }
-      number_of_staying_pages = pages.count { |page| page.data['guide_version'] == '2.3' || page.data['group'].nil? }
+      migrated_pages = pages.select { |page| page.data['layout'] and page.data['layout'].include? 'migrated' }
+      v2_3_pages = pages.select { |page| page.data['guide_version'] == '2.3' }
+      remained_pages = pages - v2_3_pages
+      deprecated_pages = remained_pages.select { |page| page.data['group'].nil? || page.data['redirect_to'] }
+      all_migrating_pages = remained_pages - deprecated_pages
+      remained_migrating_pages = all_migrating_pages - migrated_pages
       migrated_pages_data = []
 
+      # Create an array of JSON objects that contain metadata for migrated pages
       migrated_pages.each do |page|
         migrated_page = {
           path: page.path,
@@ -38,8 +42,10 @@ module Jekyll
         migrated_pages_data << migrated_page
       end
 
+      # Group migrated pages by guide
       migrated_pages_by_group = migrated_pages_data.group_by { |page| page[:guide] }.sort.to_h
-      content = "The folowing #{migrated_pages.size} topics out of #{pages.size - number_of_staying_pages} have been migrated and will be redirected soon.\n\n"
+      # Introductory text in the Migrated topics page 
+      content = "The folowing #{migrated_pages.size} topics out of #{all_migrating_pages.size} have been migrated and will be redirected soon.\n\n"
       migrated_pages_by_group.each do |guide, topics|
         content += "\n## #{guide}\n\n\n"
         topics.sort_by { |topic| topic[:title] }
@@ -68,6 +74,16 @@ module Jekyll
       # Add the newly constructed page object to the rest of pages
       # on the site.
       pages << topic
+
+      site.data['migration'] =
+            {
+              'migrated_pages' => migrated_pages.map(&:path),
+              'deprecated_pages' => deprecated_pages.map(&:path),
+              'all_migrating_pages' => all_migrating_pages.map(&:path),
+              'remained_migrating_pages' => remained_migrating_pages.map(&:path)
+            }
+
+      migrated_pages_data
     end
   end
 end
